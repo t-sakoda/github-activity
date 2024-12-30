0371723b969d47f89528e9264533af31e1b4f3b5
Sakoda, Taro <sakoda.taro.229@gmail.com>
2024-12-30 16:50:45 +0900

    ✨ Implement command-line argument parsing for GitHub username

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg()]
+    username: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let url = format!("https://api.github.com/users/{}/events", args.username);
+    println!("URL: {}", url);
 }
