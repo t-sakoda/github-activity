942e94fc2ed6078e9806a5810b9f00d1c739d672
Sakoda, Taro <sakoda.taro.229@gmail.com>
2024-12-30 17:35:56 +0900

    🚧 デバッグ出力を追加して、URLとレスポンスボディを表示するように変更

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let url = format!("https://api.github.com/users/{}/events", args.username);
-    println!("URL: {}", url);
+    dbg!("URL: {}", &url);
 
     let client = reqwest::Client::builder()
         .user_agent(APP_USER_AGENT)
@@ -22,7 +22,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let response = client.get(&url).send().await?;
     let body = response.text().await?;
-    println!("Body: {}", body);
+    dbg!(&body);
+
+    let obj: serde_json::Value = serde_json::from_str(&body)?;
+    dbg!(&obj);
 
     Ok(())
 }
