aa13df40c425deaff6bbb23a1772ef97faeaf5ef
Sakoda, Taro <sakoda.taro.229@gmail.com>
2024-12-30 17:10:03 +0900

    🚧  Refactor main function to use async/await and add user agent for HTTP requests

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,22 @@ struct Args {
     username: String,
 }
 
-fn main() {
+static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let url = format!("https://api.github.com/users/{}/events", args.username);
     println!("URL: {}", url);
+
+    let client = reqwest::Client::builder()
+        .user_agent(APP_USER_AGENT)
+        .build()?;
+
+    let response = client.get(&url).send().await?;
+    let body = response.text().await?;
+    println!("Body: {}", body);
+
+    Ok(())
 }
